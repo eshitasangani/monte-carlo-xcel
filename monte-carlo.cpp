d17d967c345dca7c0f7ef62c7327b0ca53561c31
akc55 <akc55@cornell.edu>
2023-11-07 17:48:38 -0500

    templated function and typedef

--- monte-carlo.cpp
+++ monte-carlo.cpp
@@ -1,37 +1,24 @@
 #include <iostream>
-
-// Function to generate a random number in the range [0, 1)
-double generateRandomNumber(unsigned int& seed, int size, unsigned int feedbackMask) {
-    // Generate the next bit in the LFSR sequence
-    bool nextBit = seed & 1;
-    seed >>= 1;
-    if (nextBit) {
-        seed ^= feedbackMask;
-    }
-
-    // Convert the LFSR output to a random number in the range [0, 1)
-    return static_cast<double>(seed) / ((1U << size) - 1);
-}
-
+typedef double theta_type;
 // A simple implementation of the max
 template <typename T>
 T custom_max(T a, T b)
 {
   return (b < a) ? a : b;
 }
-
-double custom_abs(double x) {
+template <typename T>
+T custom_abs(T x) {
     return (x < 0) ? -x : x;
 }
-
-double custom_sqrt(double x, double epsilon) {
+template <typename T>
+T custom_sqrt(T x, T epsilon) {
     if (x < 0) {
         return -1.0;
     }
-    double guess = x;
-    double nextGuess = 0.5 * (guess + x / guess);
+    T guess = x;
+    T nextGuess = 0.5 * (guess + x / guess);
 
-    while (custom_abs(guess - nextGuess) >= epsilon) {
+    while (custom_abs<T>(guess - nextGuess) >= epsilon) {
         guess = nextGuess;
         nextGuess = 0.5 * (guess + x / guess);
     }
@@ -52,7 +39,7 @@ T custom_log(T x, int terms)
   T term = (x - 1) / (x + 1);
   T term_squared = term * term;
   T numerator = term;
-  int denominator = 1;
+  T denominator = 1;
 
   for (int i = 1; i <= terms; i++)
   {
@@ -65,7 +52,7 @@ T custom_log(T x, int terms)
 }
 
 template <typename T>
-T custom_exp(T x, int terms)
+T custom_exp(T x, int terms = 10)
 {
   T result = 1.0;
   T term = 1.0;
@@ -84,75 +71,77 @@ T custom_exp(T x, int terms)
 // gaussian random numbers - necessary for the Monte Carlo method below
 // Note that C++11 actually provides std::normal_distribution<> in
 // the <random> library, which can be used instead of this function
-double gaussian_box_muller()
+theta_type gaussian_box_muller()
 {
-  double x = 0.0;
-  double y = 0.0;
-  double euclid_sq = 0.0;
-  double epsilon = 0.00001;
+  theta_type x = 0.0;
+  theta_type y = 0.0;
+  theta_type euclid_sq = 0.0;
+  theta_type epsilon = 0.00001;
 
   // Continue generating two uniform random variables
   // until the square of their "euclidean distance"
   // is less than unity
   do
   {
-    x = 2.0 * rand() / static_cast<double>(RAND_MAX) - 1;
-    y = 2.0 * rand() / static_cast<double>(RAND_MAX) - 1;
+    theta_type randMax = RAND_MAX;
+    x = 2.0 * rand() / randMax - 1;
+    y = 2.0 * rand() / randMax - 1;
     euclid_sq = x * x + y * y;
   } while (euclid_sq >= 1.0);
 
-  return x * custom_sqrt(-2 * custom_log(euclid_sq, 10) / euclid_sq, epsilon);
+  return x * custom_sqrt<theta_type>(-2 * custom_log<theta_type>(euclid_sq, 10) / euclid_sq, epsilon);
 }
 
 // Pricing a European vanilla call option with a Monte Carlo method
-double monte_carlo_call_price(const int &num_sims, const double &S, const double &K, const double &r, const double &v, const double &T)
+theta_type monte_carlo_call_price(const int &num_sims, const theta_type &S, const theta_type &K, const theta_type &r, const theta_type &v, const theta_type &T)
 {
-  double S_adjust = S * custom_exp(T * (r - 0.5 * v * v), 10);
-  double S_cur = 0.0;
-  double payoff_sum = 0.0;
-  double epsilon = 0.0001;
+  theta_type S_adjust = S * custom_exp<theta_type>(T * (r - 0.5 * v * v));
+  theta_type S_cur = 0.0;
+  theta_type payoff_sum = 0.0;
+  theta_type epsilon = 0.0001;
 
   for (int i = 0; i < num_sims; i++)
   {
-    double gauss_bm = gaussian_box_muller();
-    S_cur = S_adjust * custom_exp(custom_sqrt(v * v * T, epsilon) * gauss_bm, 10);
-    payoff_sum += custom_max<double>(S_cur - K, 0.0);
+    theta_type gauss_bm = gaussian_box_muller();
+    S_cur = S_adjust * custom_exp<theta_type>(custom_sqrt<theta_type>(v * v * T, epsilon) * gauss_bm);
+    payoff_sum += custom_max<theta_type>(S_cur - K, 0.0);
   }
-
-  return (payoff_sum / static_cast<double>(num_sims)) * custom_exp(-r * T, 10);
+  theta_type cast_num_sims = num_sims;
+  return (payoff_sum / cast_num_sims) * custom_exp<theta_type>(-r * T);
 }
 
 // Pricing a European vanilla put option with a Monte Carlo method
-double monte_carlo_put_price(const int &num_sims, const double &S, const double &K, const double &r, const double &v, const double &T)
+theta_type monte_carlo_put_price(const int &num_sims, const theta_type &S, const theta_type &K, const theta_type &r, const theta_type &v, const theta_type &T)
 {
-  double S_adjust = S * custom_exp(T * (r - 0.5 * v * v), 10);
-  double S_cur = 0.0;
-  double payoff_sum = 0.0;
-  double epsilon = 0.0001;
+  theta_type S_adjust = S * custom_exp<theta_type>(T * (r - 0.5 * v * v));
+  theta_type S_cur = 0.0;
+  theta_type payoff_sum = 0.0;
+  theta_type epsilon = 0.0001;
 
   for (int i = 0; i < num_sims; i++)
   {
-    double gauss_bm = gaussian_box_muller();
-    S_cur = S_adjust * custom_exp(custom_sqrt(v * v * T, epsilon) * gauss_bm, 10);
-    payoff_sum += custom_max<double>(K - S_cur, 0.0);
+    theta_type gauss_bm = gaussian_box_muller();
+    S_cur = S_adjust * custom_exp<theta_type>(custom_sqrt<theta_type>(v * v * T, epsilon) * gauss_bm);
+    payoff_sum += custom_max<theta_type>(K - S_cur, 0.0);
   }
 
-  return (payoff_sum / static_cast<double>(num_sims)) * custom_exp(-r * T, 10);
+  theta_type cast_num_sims = num_sims;
+  return (payoff_sum / cast_num_sims) * custom_exp<theta_type>(-r * T);
 }
 
 int main(int argc, char **argv)
 {
   // First we create the parameter list
   int num_sims = 10000000; // Number of simulated asset paths
-  double S = 100.0;        // Option price
-  double K = 100.0;        // Strike price
-  double r = 0.05;         // Risk-free rate (5%)
-  double v = 0.2;          // Volatility of the underlying (20%)
-  double T = 1.0;          // One year until expiry
+  theta_type S = 100.0;        // Option price
+  theta_type K = 100.0;        // Strike price
+  theta_type r = 0.05;         // Risk-free rate (5%)
+  theta_type v = 0.2;          // Volatility of the underlying (20%)
+  theta_type T = 1.0;          // One year until expiry
 
   // Then we calculate the call/put values via Monte Carlo
-  double call = monte_carlo_call_price(num_sims, S, K, r, v, T);
-  double put = monte_carlo_put_price(num_sims, S, K, r, v, T);
+  theta_type call = monte_carlo_call_price(num_sims, S, K, r, v, T);
+  theta_type put = monte_carlo_put_price(num_sims, S, K, r, v, T);
 
   // Finally we output the parameters and prices
   std::cout << "Number of Paths: " << num_sims << std::endl;
@@ -164,17 +153,6 @@ int main(int argc, char **argv)
 
   std::cout << "Call Price:      " << call << std::endl;
   std::cout << "Put Price:       " << put << std::endl;
-
-   unsigned int seed = 0b101101;  // Initial seed
-    int size = 6;  // LFSR size
-    unsigned int feedbackMask = 0b100101;  // Feedback mask (adjust for desired behavior)
-
-    for (int i = 0; i < 10; i++) {
-        double randomNumber = generateRandomNumber(seed, size, feedbackMask);
-        std::cout << randomNumber << " ";
-    }
-
-    std::cout << std::endl;
 
   return 0;
 }
