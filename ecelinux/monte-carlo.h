14d443d6b0c6cdfc4845f50f9075a4e94ec85d2f
Eshita Sangani <ens57@cornell.edu>
2023-12-14 16:52:50 -0500

    Update monte-carlo.h

--- ecelinux/monte-carlo.h
+++ ecelinux/monte-carlo.h
@@ -12,14 +12,6 @@
 typedef struct result_type result_type;
 
 typedef float theta_type;
-
-// int num_sims = 1000000;      // Number of simulated asset paths
-// theta_type S = 100.0;        // Option price
-// theta_type K = 100.0;        // Strike price
-// theta_type r = 0.05;         // Risk-free rate (5%)
-// theta_type v = 0.2;          // Volatility of the underlying (20%)
-// theta_type T = 1.0;          // One year until expiry
-
 extern int num_sims;
 extern theta_type S, K, r, v, T;
 
